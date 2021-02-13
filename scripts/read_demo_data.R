98b4bd5dc7c2c23a6841518de472a4be8d085ca8
cngupta <chiraggupta.bioinfo@gmail.com>
2021-02-13 07:40:40 -0600

    correct read demo

--- scripts/read_demo_data.R
+++ scripts/read_demo_data.R
@@ -10,7 +10,8 @@ genome_data="./demo_data/genome"
 
 #Read genome data
 TFs=read.table(paste(genome_data,"TF_names_v_1.01.txt",sep="/"))
-GOBP=read.table(paste(genome_data,"Human_GO_bp_with_GO_iea_symbol.mt3lt500.genesets",sep="/"))
+GOBP=read.table(paste(genome_data,"Human_GO_bp_with_GO_iea_symbol.mt3lt500.genesets",sep="/"), quote="")
+GOBP.gs=loadGSC(GOBP)
 
 #Read network data; careful of headers
 ex_genie=read.table(paste(net_data,"ex_genie.demo.txt",sep="/"))
